349e921cd6cbc69e0973f2e865ac797d00a13a94
Caelan van Eijnsbergen <caelan.van.eijnsbergen@gmail.com>
2020-09-16 20:06:48 +0200

    Updated SubRedditImageGenerator

--- src/main/java/APIHandler.java
+++ src/main/java/APIHandler.java
@@ -1,23 +1,42 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 import org.json.JSONObject;
 
+import java.awt.*;
+
 public class APIHandler {
 
     public APIHandler() {
     }
 
-    public String SubRedditImageGenerator(String subReddit){
+    public EmbedBuilder SubRedditImageGenerator(String subReddit){
+
+        EmbedBuilder eb = new EmbedBuilder();
+        EmbedBuilder eb2 = new EmbedBuilder();
+        eb.setColor(new Color(66, 135, 245));
+        eb2.setColor(new Color(66, 135, 245));
 
         try {
             JSONObject jsonObject = getAPI("http://h2892166.stratoserver.net/api/meme/?subs=" + subReddit);
-            String imageUrl = jsonObject.getString("result");
+            JSONObject jsonData = jsonObject.getJSONObject("article");
+            String title = jsonData.getString("title");
+            String imageUrl = jsonData.getString("image");
+            String upvotes = String.valueOf(jsonData.getInt("upvotes"));
 
-            return imageUrl;
+            eb.setTitle(title);
+            eb.setImage(imageUrl);
+            eb.setFooter("Upvotes: " + upvotes);
+
+            return eb;
+
         } catch(Exception e){
-            return "Random sub image not found.";
+            eb2.setTitle("This sub probably doesn't exist, or I'm just a moron.");
+
+            return eb2;
         }
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import music.JoinCommand;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -8,7 +9,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
 
-    private static final String DiscordToken = "NzU1MDc3NDM1Mjg4MDYwMDg1.X1-CqQ.Jhl-dWEXyNzMyNlymKNww5jS2ZU";
+    private static final String DiscordToken = "NzU1MDc3NDM1Mjg4MDYwMDg1.X1-CqQ.mtlldzmFRR14uT67YxxWk09mvKM";
     private static JDA jda;
 
     private final String commandBeginning = "!";
@@ -55,8 +56,8 @@ public class Main extends ListenerAdapter {
         }
 
         else if(message.toLowerCase().contains("sub ")) {
-            String sub = new APIHandler().SubRedditImageGenerator(message.substring(4));
-            event.getChannel().sendMessage(sub).queue();
+            EmbedBuilder sub = new APIHandler().SubRedditImageGenerator(message.substring(4));
+            event.getChannel().sendMessage(sub.build()).queue();
         }
 
         else if(message.toLowerCase().contains("music")){
